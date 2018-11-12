0948a0f9c377dd806963b6fb0c19286750b39708
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-12 21:28:55 +0700

    :tada: Init frontend & api.

---
+++ api/src/test/java/com/sit/softwareprocess/EcommerceProject/EcommerceProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sit.softwareprocess.EcommerceProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommerceProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
