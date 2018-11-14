f64a1ebb3a93a8a6042e3eed4d42d41d332fcce2
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-14 12:47:03 +0700

    [ADD] Method getUserById in controller.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/User/UserController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/User/UserController.java
@@ -1,8 +1,23 @@
 package com.sit.softwareprocess.EcommerceProject.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/users/{id}"
+    )
+    public ResponseEntity<User> getUserById(@PathVariable("id") int id){
+        User user = userService.getUserById(id);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 
 }
