932ae8d6b7eca247e4c296eab8b64878c5b9f3ae
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-14 12:43:40 +0700

    [ADD] Method getUserById.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/User/UserService.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/User/UserService.java
@@ -1,6 +1,16 @@
 package com.sit.softwareprocess.EcommerceProject.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User getUserById(int id){
+         return userRepository.getOne(id);
+    }
+
 }
