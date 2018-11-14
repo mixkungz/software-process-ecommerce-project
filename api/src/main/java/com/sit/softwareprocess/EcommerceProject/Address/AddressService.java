100b93d66d26ef393b337588ab2c4f847916acf3
Unknown <thanatta.o@mail.kmutt.ac.th>
2018-11-14 13:44:05 +0700

    [ADD] AddressService.

---
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Address/AddressService.java
@@ -0,0 +1,12 @@
+package com.sit.softwareprocess.EcommerceProject.Address;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    AddressRepository addressRepository;
+
+}
