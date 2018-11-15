937c4143e34e74d1184cd757d0ba3cbe8bf05aeb
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-15 20:53:22 +0700

    [ADD] Path /payments for getAllPayments.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentController.java
@@ -1,10 +1,25 @@
 package com.sit.softwareprocess.EcommerceProject.Payment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PaymentController {
     @Autowired
     PaymentService paymentService;
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/payments"
+    )
+    public ResponseEntity<List<Payment>> getAllPayments(){
+        List<Payment> payments = paymentService.getAllPayments();
+        return new ResponseEntity<List<Payment>>(payments, HttpStatus.OK);
+    }
 }
