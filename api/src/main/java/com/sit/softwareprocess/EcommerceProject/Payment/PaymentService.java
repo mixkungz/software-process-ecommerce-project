1667d3cdd922f1406a90108dd1ebf71b10616e36
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-15 20:48:34 +0700

    [ADD] Method getAllPayment.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentService.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentService.java
@@ -3,8 +3,14 @@ package com.sit.softwareprocess.EcommerceProject.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PaymentService {
     @Autowired
     PaymentRepository paymentRepository;
+
+    public List<Payment> getAllPayment(){
+        return paymentRepository.findAll();
+    }
 }
