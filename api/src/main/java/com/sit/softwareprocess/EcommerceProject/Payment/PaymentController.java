d882bd8887a6382f6b8493eab5e9277a9a814ebb
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-15 20:55:13 +0700

    [ADD] Path /payments/{id} for getPaymentById.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentController.java
@@ -3,6 +3,7 @@ package com.sit.softwareprocess.EcommerceProject.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,14 @@ public class PaymentController {
     public ResponseEntity<List<Payment>> getAllPayments(){
         List<Payment> payments = paymentService.getAllPayments();
         return new ResponseEntity<List<Payment>>(payments, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/payments/{id}"
+    )
+    public ResponseEntity<Payment> getPaymentById(@PathVariable("id") int id){
+        Payment payment = paymentService.getPaymentById(id);
+        return new ResponseEntity<Payment>(payment,HttpStatus.OK);
     }
 }
