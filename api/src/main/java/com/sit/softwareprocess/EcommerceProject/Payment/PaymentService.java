9bd9d5242b8b2ce1a41f1f46556da4f8e8729e8a
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-15 20:47:49 +0700

    [ADD] Payment service.

---
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Payment/PaymentService.java
@@ -0,0 +1,10 @@
+package com.sit.softwareprocess.EcommerceProject.Payment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    @Autowired
+    PaymentRepository paymentRepository;
+}
