aafed1f306cc46b4cc190b2c8b02aac10be09f6a
Purich Sangprasert <purich.sangprasert@mail.kmutt.ac.th>
2018-11-16 12:29:53 +0700

    [ADD] ShippingRepository

---
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Shipping/ShippingRepository.java
@@ -0,0 +1,8 @@
+package com.sit.softwareprocess.EcommerceProject.Shipping;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShippingRepository extends JpaRepository<Shipping,Integer> {
+}
