c4fda9dca6d1bc7ae0ce8acd60860e66c1076d27
Purich Sangprasert <purich.sangprasert@mail.kmutt.ac.th>
2018-11-16 12:53:29 +0700

    [ADD] getShippingType Function in ShippingController

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Shipping/ShippingController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Shipping/ShippingController.java
@@ -2,7 +2,12 @@ package com.sit.softwareprocess.EcommerceProject.Shipping;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class ShippingController {
@@ -10,5 +15,14 @@ public class ShippingController {
     @Autowired
     ShippingService shippingService;
 
-    
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/shipping"
+    )
+
+    public ResponseEntity <List<Shipping>> getAllShippingType (){
+        List <Shipping> shippings = shippingService.getAllShippingType();
+        return new ResponseEntity<List<Shipping>>(shippings, HttpStatus.OK);
+    }
+
 }
