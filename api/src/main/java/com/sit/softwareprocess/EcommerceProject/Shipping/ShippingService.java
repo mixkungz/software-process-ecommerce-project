9ad0858ee9f384821d7f3f5a37dc877334bc1441
Purich Sangprasert <purich.sangprasert@mail.kmutt.ac.th>
2018-11-16 12:46:31 +0700

    [ADD] getAllShipping Function in ShippingService

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Shipping/ShippingService.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Shipping/ShippingService.java
@@ -2,6 +2,7 @@ package com.sit.softwareprocess.EcommerceProject.Shipping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ShippingService {
@@ -9,5 +10,9 @@ public class ShippingService {
     @Autowired
     ShippingRepository shippingRepository;
 
+    public List<Shipping> getAllShipping(){
 
+        List<Shipping> shipping = shippingRepository.findAll();
+        return shipping;
+    }
 }
