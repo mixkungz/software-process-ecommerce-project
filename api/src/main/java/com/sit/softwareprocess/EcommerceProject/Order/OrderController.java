0b1f4d33640f599b2d2018284ceb0722d1cfd56a
Thanatta J <thanatta.o@mail.kmutt.ac.th>
2018-11-18 22:11:12 +0700

    [ADD] Function createOrder in order controller.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Order/OrderController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Order/OrderController.java
@@ -1,12 +1,91 @@
 package com.sit.softwareprocess.EcommerceProject.Order;
 
+import com.google.gson.*;
+import com.sit.softwareprocess.EcommerceProject.Address.Address;
+import com.sit.softwareprocess.EcommerceProject.Address.AddressService;
+import com.sit.softwareprocess.EcommerceProject.OrderDetail.OrderDetail;
+import com.sit.softwareprocess.EcommerceProject.OrderDetail.OrderDetailService;
+import com.sit.softwareprocess.EcommerceProject.Payment.Payment;
+import com.sit.softwareprocess.EcommerceProject.Product.Product;
+import com.sit.softwareprocess.EcommerceProject.Product.ProductService;
+import com.sit.softwareprocess.EcommerceProject.Shipping.Shipping;
+import com.sit.softwareprocess.EcommerceProject.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Date;
+
 
 @RestController
 public class OrderController {
 
     @Autowired
     OrderService orderService;
-    
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    OrderDetailService orderDetailService;
+
+    @Autowired
+    AddressService addressService;
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/createOrder"
+    )
+        public ResponseEntity<Order> createOrder(
+            @RequestBody String jsonData
+    ){
+
+        Gson gson = new Gson();
+
+        JsonElement jsonElement = new JsonParser().parse(jsonData);
+        JsonObject  jsonObject = jsonElement.getAsJsonObject();
+
+        JsonObject orderJson = jsonObject.getAsJsonObject("order");
+        JsonObject addressJson = jsonObject.getAsJsonObject("address");
+        JsonObject shippingJson = jsonObject.getAsJsonObject("shipping");
+        JsonObject paymentJson = jsonObject.getAsJsonObject("payment");
+        JsonObject userJson = jsonObject.getAsJsonObject("user");
+        JsonArray productJson = jsonObject.getAsJsonArray("product");
+        JsonArray amountJson = jsonObject.getAsJsonArray("amount");
+
+        Order order = gson.fromJson(orderJson,Order.class);
+        Address address = gson.fromJson(addressJson,Address.class);
+        Shipping shipping = gson.fromJson(shippingJson,Shipping.class);
+        Payment payment = gson.fromJson(paymentJson,Payment.class);
+        User user = gson.fromJson(userJson,User.class);
+        int[] productList = gson.fromJson(productJson,int[].class);
+        int[] amount = gson.fromJson(amountJson,int[].class);
+
+        int totalAmount = 0;
+        if(amount.length == productList.length) {
+            for (int qty : amount) {
+                totalAmount += qty;
+            }
+        }else{
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        Address addressCreate = addressService.createAddress(address);
+        order.setAddress(addressCreate);
+        order.setShipping(shipping);
+        order.setPayment(payment);
+        order.setUser(user);
+        order.setAmount(totalAmount);
+        order.setCreateAt(new Date());
+
+        Order orderCreate = orderService.createOrder(order);
+
+        for (int i = 0; i< productList.length; i++){
+            Product product = productService.getProductBySku(productList[i]);
+            OrderDetail orderDetail = orderDetailService.createOrderDetail(orderCreate.getId(),product.getId(),amount[i]);
+        }
+
+        return new ResponseEntity<Order>(orderCreate, HttpStatus.OK);
+    }
+
 }
