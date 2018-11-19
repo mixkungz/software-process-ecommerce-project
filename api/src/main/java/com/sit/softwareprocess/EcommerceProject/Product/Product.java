70dfb7ee0a0dcc0e88a6507021036a3133854063
Thanatta J <thanatta.o@mail.kmutt.ac.th>
2018-11-19 15:46:20 +0700

    [UPDATE] Product model.
    change type method

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Product/Product.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Product/Product.java
@@ -90,11 +90,11 @@ public class Product implements Serializable {
         this.productPrice = productPrice;
     }
 
-    public int getSku() {
+    public String getSku() {
         return sku;
     }
 
-    public void setSku(int sku) {
+    public void setSku(String sku) {
         this.sku = sku;
     }
 
@@ -146,11 +146,11 @@ public class Product implements Serializable {
         this.length = length;
     }
 
-    public int getSize() {
+    public String getSize() {
         return size;
     }
 
-    public void setSize(int size) {
+    public void setSize(String size) {
         this.size = size;
     }
 
