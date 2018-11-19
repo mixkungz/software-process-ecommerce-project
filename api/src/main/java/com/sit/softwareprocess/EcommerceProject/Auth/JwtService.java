9d4f5bf399295384bc3ccee16a8c429593f10303
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-19 16:42:13 +0700

    [UPDATE] Change scope variable to private.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Auth/JwtService.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Auth/JwtService.java
@@ -1,9 +1,11 @@
 package com.sit.softwareprocess.EcommerceProject.Auth;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,7 @@ import java.util.Date;
 public class JwtService {
 
     @Value("${jwt.secret}")
-    String secretKey;
+    private String secretKey;
 
     private JwtModel jwtModel;
 
@@ -39,5 +41,7 @@ public class JwtService {
             return null;
         }
     }
+
+
 
 }
