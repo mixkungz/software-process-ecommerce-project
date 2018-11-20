884d5d4c30cd67aacd47dc8352e47b8822eb648d
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-20 13:42:56 +0700

    [ADD] Facebook login controller.

--- api/src/main/java/com/sit/softwareprocess/EcommerceProject/Auth/AuthController.java
+++ api/src/main/java/com/sit/softwareprocess/EcommerceProject/Auth/AuthController.java
@@ -25,6 +25,8 @@ public class AuthController {
         JwtModel jwtModel = jwtService.generateJwtToken(user.getId());
         return new ResponseEntity<JwtModel>(jwtModel,HttpStatus.CREATED);
     }
+
+
     @CrossOrigin("*")
     @RequestMapping(
             method = RequestMethod.POST,
@@ -32,6 +34,31 @@ public class AuthController {
     )
     public boolean checkToken(@RequestHeader(name = "Authorization", required = true) String token){
         return jwtService.isTokenExpire(token);
+    }
+
+    @CrossOrigin("*")
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/auth/login/facebook"
+    )
+    public ResponseEntity<JwtModel> loginWithFacebook(@RequestParam("facebookId") String facebookId,@RequestParam("firstName")String firstName,@RequestParam("lastName")String lastName){
+        User user = userService.getUserByFacebookId(facebookId);
+        if(!hasUser(user)){
+            System.out.println("no user");
+            user = new User();
+            user.setAdmin(false);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setFacebookId(facebookId);
+            user.setEmail("mockup@email.com");
+            user.setUsername("%^&A*S(&CP<}D{PCOIABSVD"+facebookId+"AXB&*EQYAD%^R@&TE*Y*(U(PICNAISBD()CBCAUS(DI_)");
+            user.setPassword("ABSC*&)N_(MDIN)DCABS(N+D_M+)D(_)(U*Y&T^FTVUAYBUCNIPS{K}LD"+facebookId+"AVRSAM)_ASCD(&*AST*()CD_})*(&*^^$%#$EUARCSVITBDUIP");
+            user.setTelephoneNumber("191");
+            user = userService.createUser(user);
+        }
+        JwtModel jwtModel = jwtService.generateJwtToken(user.getId());
+        System.out.println();
+        return new ResponseEntity<JwtModel>(jwtModel,HttpStatus.CREATED);
     }
 
     public boolean hasUser(User user){
